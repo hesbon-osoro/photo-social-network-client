8f08cda1e6fbae80a60f2b2178a621cb4e4df01d
wazimuhb <hesbonosoro1@gmail.com>
2021-12-07 22:44:03 +0300

    Passing dummy data to Post reusable component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Post from './components/post/Post';
 
 function App() {
-	return <div>Photo Social App</div>;
+	const [posts, setPosts] = useState([
+		{
+			username: 'wazimu',
+			caption: 'awesome websites using MERNG stack',
+			imageUrl:
+				'https://www.techlifediary.com/wp-content/uploads/2020/06/react-js.png',
+		},
+		{
+			username: 'hesbon',
+			caption: 'Such a beautiful world',
+			imageUrl:
+				'https://quotefancy.com/media/wallpaper/3840x2160/126631-Charles-Dickens-Quote-And-a-beautiful-world-you-live-in-when-it-is.jpg',
+		},
+	]);
+	return (
+		<div className="app">
+			<div className="app__header">
+				<img className="app__headerImage" src="logo192.png" alt="Header" />
+			</div>
+			{posts.map(({ username, caption, imageUrl }) => (
+				<Post username={username} caption={caption} imageUrl={imageUrl} />
+			))}
+		</div>
+	);
 }
 
 export default App;
