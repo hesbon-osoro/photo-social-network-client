e1cec1025517ef1c6e207d296552645821272124
wazimuhb <hesbonosoro1@gmail.com>
2021-12-07 22:24:50 +0300

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyB36Jgoh1F8Ea-YmamkZsOhFavdi3V-eLQ',
+	authDomain: 'photo-social-network.firebaseapp.com',
+	projectId: 'photo-social-network',
+	storageBucket: 'photo-social-network.appspot.com',
+	messagingSenderId: '653469983531',
+	appId: '1:653469983531:web:2432872b933e1507e19b6e',
+	measurementId: '${config.measurementId}',
+};
