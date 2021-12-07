91e57e02a702d1cf993253f2258a3b42cf501883
wazimuhb <hesbonosoro1@gmail.com>
2021-12-07 23:16:40 +0300

    firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyB36Jgoh1F8Ea-YmamkZsOhFavdi3V-eLQ',
 	authDomain: 'photo-social-network.firebaseapp.com',
@@ -7,3 +9,10 @@ const firebaseConfig = {
 	appId: '1:653469983531:web:2432872b933e1507e19b6e',
 	measurementId: '${config.measurementId}',
 };
+
+const firebaseApp=firebase.initializeApp(firebaseConfig)
+const db=firebaseApp.firestore()
+const auth=firebase.auth();
+const storage=firebase.storage()
+
+export {db,auth,storage} 
