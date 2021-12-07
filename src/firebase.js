0e2ece049c2082ee59312ed0585b5ae73a371c61
wazimuhb <hesbonosoro1@gmail.com>
2021-12-07 23:57:15 +0300

    import correction

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
-import firebase from 'firebase';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"; //for authentication
+import "firebase/compat/storage"; //for storage
+import "firebase/compat/database"; //for realtime
+import "firebase/compat/firestore"; //for cloud firestore
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyB36Jgoh1F8Ea-YmamkZsOhFavdi3V-eLQ',
